f51e4130a4a9f9b02e1eb95c263203426c076761
Marianna Zuyeva <moraofcrims@gmail.com>
2026-05-19 03:57:32 +0500

    use port 465

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
